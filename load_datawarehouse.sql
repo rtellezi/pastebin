7f720f80036835b49c4d1c225cd3e1d2e57d8619
Roberto Tellez Ibarra <rtellezi@gmail.com>
2023-05-09 23:42:07 -0400

    ELT DWH with Synapse

---
+++ load_datawarehouse.sql
@@ -0,0 +1,104 @@
+-- Load data from a data lake by using the COPY statement
+
+SELECT COUNT(1) 
+FROM dbo.StageProduct
+
+COPY INTO dbo.StageProduct
+    (ProductID, ProductName, ProductCategory, Color, Size, ListPrice, Discontinued)
+FROM 'https://datalake7d03kur.blob.core.windows.net/files/data/Product.csv'
+WITH
+(
+    FILE_TYPE = 'CSV',
+    MAXERRORS = 0,
+    IDENTITY_INSERT = 'OFF',
+    FIRSTROW = 2 --Skip header row
+);
+
+
+SELECT COUNT(1) 
+FROM dbo.StageProduct
+
+COPY INTO dbo.StageCustomer
+(GeographyKey, CustomerAlternateKey, Title, FirstName, MiddleName, LastName, NameStyle, BirthDate, 
+MaritalStatus, Suffix, Gender, EmailAddress, YearlyIncome, TotalChildren, NumberChildrenAtHome, EnglishEducation, 
+SpanishEducation, FrenchEducation, EnglishOccupation, SpanishOccupation, FrenchOccupation, HouseOwnerFlag, 
+NumberCarsOwned, AddressLine1, AddressLine2, Phone, DateFirstPurchase, CommuteDistance)
+FROM 'https://datalake7d03kur.dfs.core.windows.net/files/data/Customer.csv'
+WITH
+(
+FILE_TYPE = 'CSV'
+,MAXERRORS = 5
+,FIRSTROW = 2 -- skip header row
+,ERRORFILE = 'https://datalake7d03kur.dfs.core.windows.net/files/'
+);
+
+SELECT *
+FROM dbo.StageCustomer;
+
+-- Use a CREATE TABLE AS (CTAS) statement
+
+CREATE TABLE dbo.DimProduct
+WITH
+(
+    DISTRIBUTION = HASH(ProductAltKey),
+    CLUSTERED COLUMNSTORE INDEX
+)
+AS
+SELECT ROW_NUMBER() OVER(ORDER BY ProductID) AS ProductKey,
+    ProductID AS ProductAltKey,
+    ProductName,
+    ProductCategory,
+    Color,
+    Size,
+    ListPrice,
+    Discontinued
+FROM dbo.StageProduct;
+
+SELECT ProductKey,
+    ProductAltKey,
+    ProductName,
+    ProductCategory,
+    Color,
+    Size,
+    ListPrice,
+    Discontinued
+FROM dbo.DimProduct;
+
+-- Combine INSERT and UPDATE statements to load a slowly changing dimension table
+
+INSERT INTO dbo.DimCustomer ([GeographyKey],[CustomerAlternateKey],[Title],[FirstName],[MiddleName],[LastName],[NameStyle],[BirthDate],[MaritalStatus],
+[Suffix],[Gender],[EmailAddress],[YearlyIncome],[TotalChildren],[NumberChildrenAtHome],[EnglishEducation],[SpanishEducation],[FrenchEducation],
+[EnglishOccupation],[SpanishOccupation],[FrenchOccupation],[HouseOwnerFlag],[NumberCarsOwned],[AddressLine1],[AddressLine2],[Phone],
+[DateFirstPurchase],[CommuteDistance])
+SELECT *
+FROM dbo.StageCustomer AS stg
+WHERE NOT EXISTS
+    (SELECT * FROM dbo.DimCustomer AS dim
+    WHERE dim.CustomerAlternateKey = stg.CustomerAlternateKey);
+
+-- Type 1 updates (change name, email, or phone in place)
+UPDATE dbo.DimCustomer
+SET LastName = stg.LastName,
+    EmailAddress = stg.EmailAddress,
+    Phone = stg.Phone
+FROM DimCustomer dim inner join StageCustomer stg
+ON dim.CustomerAlternateKey = stg.CustomerAlternateKey
+WHERE dim.LastName <> stg.LastName OR dim.EmailAddress <> stg.EmailAddress OR dim.Phone <> stg.Phone
+
+-- Type 2 updates (address changes triggers new entry)
+INSERT INTO dbo.DimCustomer
+SELECT stg.GeographyKey,stg.CustomerAlternateKey,stg.Title,stg.FirstName,stg.MiddleName,stg.LastName,stg.NameStyle,stg.BirthDate,stg.MaritalStatus,
+stg.Suffix,stg.Gender,stg.EmailAddress,stg.YearlyIncome,stg.TotalChildren,stg.NumberChildrenAtHome,stg.EnglishEducation,stg.SpanishEducation,stg.FrenchEducation,
+stg.EnglishOccupation,stg.SpanishOccupation,stg.FrenchOccupation,stg.HouseOwnerFlag,stg.NumberCarsOwned,stg.AddressLine1,stg.AddressLine2,stg.Phone,
+stg.DateFirstPurchase,stg.CommuteDistance
+FROM dbo.StageCustomer AS stg
+JOIN dbo.DimCustomer AS dim
+ON stg.CustomerAlternateKey = dim.CustomerAlternateKey
+AND stg.AddressLine1 <> dim.AddressLine1;
+
+-- Perform post-load optimization
+
+ALTER INDEX ALL ON dbo.DimProduct REBUILD;
+
+CREATE STATISTICS customergeo_stats
+ON dbo.DimCustomer (GeographyKey);
